50c6ba93ed387f0fef6620189688e311fe7c599c
Sufiyan Adhikari <dumbpyx@gmail.com>
2020-12-23 12:11:01 +0530

    Add test for external posting_predictors

--- beancount_import/source/generic_importer_source_test.py
+++ beancount_import/source/generic_importer_source_test.py
@@ -4,6 +4,7 @@ import pytest
 
 from .source_test import check_source_example
 from beancount.ingest.importers.csv import Importer as CSVImporter, Col
+from beancount.core.data import Transaction, Posting, Amount
 
 testdata_dir = os.path.realpath(
     os.path.join(
@@ -14,10 +15,14 @@ testdata_csv = os.path.join(testdata_dir, "csv")
 examples = [
     'test_basic',
     'test_invalid',
-    'test_training_examples'
+    'test_training_examples',
+    'test_dummy_predictor'
 ]
+# tests that should be wrapped in below DummyPostingPredictor
+wrap_dummy_predictor = ['test_dummy_predictor']
 
-importer = CSVImporter({Col.DATE: 'Date',
+def Importer():
+    return CSVImporter({Col.DATE: 'Date',
                         Col.NARRATION1: 'Description',
                         Col.AMOUNT: 'Amount',
                         },
@@ -26,9 +31,39 @@ importer = CSVImporter({Col.DATE: 'Date',
                        '"Date","Description","Amount"',
                        )
 
+class DummyPostingPredictor:
+    DUMMY_ACCOUNT = "Assets:Dummy"
+
+    def __init__(self, importer):
+        self.importer = importer
+        # move extract to _extract
+        self.importer._extract = self.importer.extract
+        self.importer.extract = self.extract_wrapper
+
+    def extract_wrapper(self, f, existing_entries):
+        entries = self.importer._extract(f, existing_entries)
+        for entry in entries:
+            if isinstance(entry, Transaction):
+                p = entry.postings[0]
+                entry.postings.append(
+                    Posting(
+                        account=self.DUMMY_ACCOUNT,
+                        units=Amount(currency=p.units.currency, number=-1*p.units.number),
+                        cost=None,
+                        price=None,
+                        flag=None,
+                        meta={},
+                    )
+                )
+        return entries
+
 
 @pytest.mark.parametrize('name', examples)
 def test_source(name: str):
+    importer = Importer()
+    # wrap the importer in dummy posting predictor
+    if name in wrap_dummy_predictor:
+        _ = DummyPostingPredictor(importer)
     check_source_example(
         example_dir=os.path.join(testdata_dir, name),
         source_spec={
